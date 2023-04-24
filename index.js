06052e19a69bcd886ec601fc103a2941948e878e
Sam <sam.camhi@gmail.com>
2023-04-24 00:19:44 -0400

    Send Fetch

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+/*fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  }),
+})
+.then(function (response) {
+    return response.json();
+})
+.then(function (object) {
+    console.log(object);
+})
+.catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+})
+;*/
+
+const submitData = (userName, userEmail) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: `${userName}`,
+          email: `${userEmail}`,
+        }),
+      })
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (object) {
+        const p = document.createElement("p");
+        p.innerHTML = object.id;
+        document.body.appendChild(p);
+      })
+      .catch(function (error) {
+        const p = document.createElement("p");
+        p.innerHTML = error.message;
+        document.body.appendChild(p);
+      })
+}
+
+//submitData("Sam","sam.camhi@gmail.com")
